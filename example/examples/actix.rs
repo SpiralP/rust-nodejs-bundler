55f249f359bd1f5f682f954f273c08570fe30b23
SpiralP <spiral.palmer@gmail.com>
2021-07-25 00:47:48 -0700

    upgrade deps

--- example/examples/actix.rs
+++ example/examples/actix.rs
@@ -2,18 +2,18 @@
 include!(concat!(env!("OUT_DIR"), "/parceljs.rs"));
 
 #[cfg(feature = "with_actix")]
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  use actix_web::{App, HttpServer};
+    use actix_web::{App, HttpServer};
 
-  println!("try http://127.0.0.1:8080/");
-  HttpServer::new(|| App::new().route("/*", PARCELJS.as_route()))
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    println!("try http://127.0.0.1:8080/");
+    HttpServer::new(|| App::new().route("/*", PARCELJS.as_route()))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
 
 #[cfg(not(feature = "with_actix"))]
 fn main() {
-  panic!("run with --features with_actix");
+    panic!("run with --features with_actix");
 }
